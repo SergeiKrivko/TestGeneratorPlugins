6445f7c4eceb22e25033777d6e6abe3e18f61440
SergeiKrivko <sergik.krivko@gmail.com>
2024-11-08 21:48:46 +0300

    Add test Newtonsoft.Json plugin

---
+++ TestPlugin.UI/TestPlugin.UI.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using TestGenerator.Shared.Types;
+
+namespace TestPlugin.UI;
+
+public partial class TestMainTab : MainTab
+{
+    public TestMainTab()
+    {
+        TabKey = "Newtonsoft.Json";
+        TabName = "Newtonsoft.Json";
+        InitializeComponent();
+
+        Block.Text = JsonConvert.SerializeObject(new {A = 1, B = 2, C = 3, D = 4, E = 5});
+    }
+}
---
+++ TestPlugin/TestPlugin.cs
@@ -0,0 +1,17 @@
+﻿using TestPlugin.UI;
+
+namespace TestPlugin;
+
+public class TestPlugin : TestGenerator.Shared.Plugin
+{
+    public TestPlugin()
+    {
+        Name = "NewPlugin";
+
+        MainTabs = [new TestMainTab()];
+        SideTabs = [];
+
+        BuildTypes = [];
+        ProjectTypes = [];
+    }
+}
